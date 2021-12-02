79b2083a44ce223a1ffa3884cb00dc08bb3ae425
Akshara-Dhoble <88190894+Akshara-Dhoble@users.noreply.github.com>
2021-12-02 20:15:24 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -24,5 +24,32 @@ function addRoom(){
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey ;
 
+    console.log("Room Name - "+ Room_names);
+    row = "<div class = 'room_name' id ="+Room_names+" onclick = 'redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+    document.getElementById("output").innerHTML = row ;
 
+  
+    });});}
+getData();
+function redirectToRoomName(name){
+    console.log(name);
+    localStorage.setItem("room_name" , name);
+    window.location = "kwitter_page.html";
+};
+function logout(){
+    localStorage.removeItem("room_name");
+    localStorage.removeItem("user_name");
+    window.location = "index.html";
+}
+
+
+
+
+function addUser(){
+  user_name = document.getElementById("user_name").value;
+  localStorage.setItem("user_name" , user_name);
+
+  window.location = "kwitter_room.html";
+  
+}
 
