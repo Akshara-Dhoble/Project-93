7915d20f975e940dcca1b63bb63caef711b1a14e
Akshara-Dhoble <88190894+Akshara-Dhoble@users.noreply.github.com>
2021-12-02 20:11:00 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,7 +12,6 @@ var firebaseConfig = {
 
 user_name = localStorage.getItem("username");
 document.getElementById("user_name").innerHTML = "Welcome" + user_name + "!" ;
-
 function addRoom(){
     var room_name = document.getElementById("room_name").value;
     firebase.database().ref("/").child(room_name).update({
@@ -24,3 +23,6 @@ function addRoom(){
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey ;
+
+
+
